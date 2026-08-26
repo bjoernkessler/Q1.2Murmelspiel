4423ee9e462e7826382f903f5753c33e359284c8
Björn Kessler <bjoern.kessler@ksf.de>
2026-08-26 13:33:42 +0200

    Dokumentation für die Klasse Loch.

--- Loch.java
+++ Loch.java
@@ -1,6 +1,10 @@
 /**
  *
- * Beschreibung
+ * Das Loch ist ein Objekt, 
+ * das eine zufällige Position innerhalb eines quadratischen Bereichs repräsentiert. 
+ * Die Größe des Bereichs wird durch die statische Variable "Groesse" bestimmt. 
+ * Die Position des Lochs wird durch die statischen Variablen "xPosition" und "yPosition" gespeichert.
+ *
  *
  * @version 1.0 vom 16.01.2025
  * @author 
